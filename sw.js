97ba9173d82c30078a3baa5b4af426cc4659e46f
igophy <lounger_hytten.5n@icloud.com>
2026-04-09 00:04:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "sus-ullandhaug-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./favicon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
